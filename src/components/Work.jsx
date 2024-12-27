3d799b8d80cccc5e018f7b039c41a4e4a34a477b
AZZOUZ Othman <113979048+vat7u@users.noreply.github.com>
2024-12-27 18:55:11 +0100

    commit

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,40 +3,22 @@ import ProjectCard from "./ProjectCard";
 
 const works = [
   {
-    imgSrc: '/images/project-1.jpg',
-    title: 'Full stack music app',
-    tags: ['API', 'MVC', 'Development'],
-    projectLink: 'https://musify-5al0.onrender.com/'
+    imgSrc: '/images/Baraka_project.png',
+    title: 'Resturant web app',
+    tags: ['Laravel', 'MVC', 'PHP'],
+    projectLink: 'https://github.com/vat7u/Riad-Baraka-project'
   },
   {
-    imgSrc: '/images/project-2.jpg',
-    title: 'Free stock photo app',
-    tags: ['API', 'SPA'],
-    projectLink: 'https://pixstock-official.vercel.app/'
+    imgSrc: '/images/Recipes project.png',
+    title: 'Recipes app',
+    tags: ['ReactJS', 'Firebase'],
+    projectLink: 'https://github.com/vat7u/Recipe-Project'
   },
   {
-    imgSrc: '/images/project-3.jpg',
-    title: 'Recipe app',
-    tags: ['Development', 'API'],
-    projectLink: ''
-  },
-  {
-    imgSrc: '/images/project-4.jpg',
-    title: 'Real state website',
-    tags: ['Web-design', 'Development'],
-    projectLink: 'https://github.com/codewithsadee-org/wealthome'
-  },
-  {
-    imgSrc: '/images/project-5.jpg',
-    title: 'eCommerce website',
-    tags: ['eCommerce', 'Development'],
-    projectLink: 'https://github.com/codewithsadee/anon-ecommerce-website'
-  },
-  {
-    imgSrc: '/images/project-6.jpg',
-    title: 'vCard Personal portfolio',
-    tags: ['Web-design', 'Development'],
-    projectLink: 'https://github.com/codewithsadee/vcard-personal-portfolio'
+    imgSrc: '/images/REST-API.png',
+    title: 'Rest API',
+    tags: ['Laravel', 'API', 'PHP'],
+    projectLink: 'https://github.com/vat7u/rest-api'
   },
 ];
 
